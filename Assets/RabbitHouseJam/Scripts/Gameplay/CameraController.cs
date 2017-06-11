2937989034e0a9ac93b7c69eadd2d4cf415889ae
Fletcher <fletcherc89@gmail.com>
2017-06-10 21:32:55 -0700

    Camera follows the rabbit

---
+++ Assets/RabbitHouseJam/Scripts/Gameplay/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Tracker;
+    public Vector2 Offset;
+
+    void Update()
+    {
+        if (this.Tracker != null)
+        {
+            this.transform.SetPosition(this.Tracker.transform.position.x + this.Offset.x, this.transform.position.y, this.Tracker.transform.position.z + this.Offset.y);
+        }
+    }
+}
