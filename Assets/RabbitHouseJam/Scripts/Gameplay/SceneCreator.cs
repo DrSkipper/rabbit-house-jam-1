1f611537ebf7d1948cfb2d1b4cb3e20d43ed3050
Fletcher <fletcherc89@gmail.com>
2017-06-10 23:37:48 -0700

    Trying to create a scene

---
+++ Assets/RabbitHouseJam/Scripts/Gameplay/SceneCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCreator : MonoBehaviour
+{
+    public string SceneName = "TestScene";
+
+    void Start()
+    {
+        SceneManager.LoadScene(this.SceneName, LoadSceneMode.Additive);
+        Scene scene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+        for (int i = 0; i < rootObjects.Length; ++i)
+        {
+            rootObjects[i].transform.parent = this.transform;
+        }
+
+        SceneManager.UnloadSceneAsync(this.SceneName);
+    }
+}
